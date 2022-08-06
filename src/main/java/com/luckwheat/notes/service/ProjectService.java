3549a48f9859b8a56063adbd9474e1a4d50140dd
Artyom Panfutov <artyom.cox@gmail.com>
2022-08-06 15:41:38 +0400

    add test for project creation

--- src/main/java/com/luckwheat/notes/service/ProjectService.java
+++ src/main/java/com/luckwheat/notes/service/ProjectService.java
@@ -1,11 +1,13 @@
 package com.luckwheat.notes.service;
 
 import com.luckwheat.notes.dto.CreateResult;
+import com.luckwheat.notes.dto.Error;
 import com.luckwheat.notes.dto.ProjectDto;
 import com.luckwheat.notes.entity.Project;
 import com.luckwheat.notes.repository.ProjectRepository;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import liquibase.repackaged.org.apache.commons.lang3.StringUtils;
 
 import javax.transaction.Transactional;
 
@@ -17,6 +19,18 @@ public class ProjectService {
 
     @Transactional
     public CreateResult<ProjectDto> create(ProjectDto projectDto) {
+        if (projectDto == null) {
+            return CreateResult.error(new Error("Can't create the project that is null"));
+        }
+
+        if (StringUtils.isEmpty(projectDto.name())) {
+            return CreateResult.error(new Error("The name of the project can't be empty"));
+        }
+
+        if (projectDto.id() != null) {
+            return CreateResult.error(new Error("The id must be null before creation. It will be assigned automatically."));
+        }
+
         final var saved = projectRepository.save(convertToEntity(projectDto));
 
         return CreateResult.success(convertToDto(saved));
---
+++ src/test/java/com/luckwheat/service/ProjectServiceTest.java
@@ -0,0 +1,51 @@
+package com.luckwheat.service;
+
+import com.luckwheat.notes.dto.ProjectDto;
+import com.luckwheat.notes.repository.ProjectRepository;
+import com.luckwheat.notes.service.ProjectService;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest
+@Testcontainers
+class ProjectServiceTest {
+
+    @Inject
+    ProjectService projectService;
+
+    @Inject
+    ProjectRepository projectRepository;
+
+    @Test
+    void testCreateProject() {
+        final var newProject = new ProjectDto(null, "name");
+
+        final var result = projectService.create(newProject);
+
+        assertTrue(result.success());
+        assertNotNull(result.body().id());
+        assertEquals(newProject.name(), result.body().name());
+    }
+
+    @Test
+    void testCreateWithNullName() {
+        final var result = projectService.create(new ProjectDto(null, null));
+
+        assertFalse(result.success());
+        assertNotNull(result.error());
+        assertEquals(0, projectRepository.count());
+    }
+
+    @Test
+    void testCreateWithNullInput() {
+        final var result = projectService.create(null);
+
+        assertFalse(result.success());
+        assertNotNull(result.error());
+        assertEquals(0, projectRepository.count());
+    }
+}
