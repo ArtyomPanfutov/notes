918902f31d171686eb00173bfae226466b8ee534
Artyom Panfutov <artyom.cox@gmail.com>
2022-12-11 00:45:37 +0400

    add auth0 api service

---
+++ src/main/java/com/luckwheat/notes/dto/auth0/UserInfo.java
@@ -0,0 +1,4 @@
+package com.luckwheat.notes.dto.auth0;
+
+public record UserInfo(String sub, String email) {
+}
---
+++ src/main/java/com/luckwheat/notes/service/Auth0ApiService.java
@@ -0,0 +1,34 @@
+package com.luckwheat.notes.service;
+
+import com.luckwheat.notes.dto.auth0.UserInfo;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.uri.UriBuilder;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import java.net.URI;
+
+@Singleton
+public class Auth0ApiService {
+
+    private static final URI USERINFO_URI = UriBuilder.of("/userinfo").build();
+
+    private final HttpClient httpClient;
+
+    @Inject
+    public Auth0ApiService(@Client("${micronaut.security.token.jwt.claims-validators.issuer}")
+                               HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    public UserInfo fetchUserInfo(String token) {
+        var request = HttpRequest.GET(USERINFO_URI)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        return httpClient.toBlocking().retrieve(request, Argument.of(UserInfo.class));
+    }
+}
