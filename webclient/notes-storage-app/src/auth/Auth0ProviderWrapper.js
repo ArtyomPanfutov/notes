321f4ef4e363f00579572b43a70192d7c705c02e
Artyom Panfutov <artyom.cox@gmail.com>
2022-12-27 19:07:26 +0400

    add callback to auth0 provider

--- webclient/notes-storage-app/src/auth/Auth0ProviderWrapper.js
+++ webclient/notes-storage-app/src/auth/Auth0ProviderWrapper.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
 const Auth0ProviderWrapper = ({ children }) => {
+  const navigate = useNavigate();
+
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+
+  const onRedirectCallback = (appState) => {
+    navigate((appState && appState.returnTo) || window.location.pathname);
+  };
 
   if (!(domain && clientId)) {
     console.error("The env variables are not set");
@@ -16,6 +23,7 @@ const Auth0ProviderWrapper = ({ children }) => {
       domain={domain}
       clientId={clientId}
       redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
       audience={audience}>
       {children}
     </Auth0Provider>
