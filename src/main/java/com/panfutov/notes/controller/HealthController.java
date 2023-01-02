5b58b0a481a84a68c87ca48705d32d4fc9a800a0
Artyom Panfutov <artyom.cox@gmail.com>
2023-01-02 16:37:34 +0400

    fix 401

--- src/main/java/com/panfutov/notes/controller/HealthController.java
+++ src/main/java/com/panfutov/notes/controller/HealthController.java
@@ -3,8 +3,12 @@ package com.panfutov.notes.controller;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+
+import static io.micronaut.security.rules.SecurityRule.IS_ANONYMOUS;
 
 @Controller("/api/health")
+@Secured(IS_ANONYMOUS)
 public class HealthController {
 
     @Get
